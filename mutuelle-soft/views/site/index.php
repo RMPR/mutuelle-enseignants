b8fd88dd098822f23a09088949e959538a58fbf2
RMPR <akoudanilo@gmail.com>
2019-01-01 16:34:43 +0100

    update Favicon

--- mutuelle-soft/views/site/index.php
+++ mutuelle-soft/views/site/index.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Gestion de la mutuelle des Enseignants';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Bienvenue sur la plateforme de gestion de la mutuelle</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
